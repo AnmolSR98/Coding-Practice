47eee7842413e07600d131d21b20484fee6fe0ce
AnmolSR98 <anmolratolcoding@gmail.com>
2023-08-13 15:14:54 -0600

    Update userActivity.sql

--- SQL/userActivity.sql
+++ SQL/userActivity.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT activity_date as day, COUNT(DISTINCT user_id) as active_users
 FROM Activity
-WHERE activity_date <= '2019-07-27' AND activity_date >= '2019-06-27'
+WHERE activity_date <= '2019-07-27' AND activity_date >= ADDDATE('2019-07-27', INTERVAL -30 DAY)
 GROUP BY day;
