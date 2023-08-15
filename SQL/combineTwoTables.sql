78e8c34e7aaf69e86350297f9526edbc8db2ea54
AnmolSR98 <anmolratolcoding@gmail.com>
2023-08-15 14:02:03 -0600

    Update combineTwoTables.sql

--- SQL/combineTwoTables.sql
+++ SQL/combineTwoTables.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT Person.firstName, Person.lastName, Address.city, Address.state
-FROM Person
-LEFT JOIN Address ON Person.personId = Address.personId; 
+SELECT P.firstName, P.lastName, A.city, A.state
+FROM Person AS P
+LEFT JOIN Address AS A ON P.personId = A.personId;
