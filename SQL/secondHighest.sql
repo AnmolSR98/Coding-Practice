33d5e1a4cbfb90a088693e1f60f1ba195c450819
AnmolSR98 <anmolratolcoding@gmail.com>
2023-08-15 17:30:20 -0600

    Update secondHighest.sql

--- SQL/secondHighest.sql
+++ SQL/secondHighest.sql
@@ -2,6 +2,7 @@
 SELECT IFNULL(
 # created a table where it will leave a blank if there is no 2nd highest
 (SELECT salary
-FROM Employee 
+FROM Employee
+# replace the value of the OFFSET to get the nth highest you desire
 ORDER BY salary DESC LIMIT 1 OFFSET 1), NULL)
 AS SecondHighestSalary;
