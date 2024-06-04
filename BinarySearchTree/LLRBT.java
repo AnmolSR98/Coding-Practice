9d94c7475c7c6b24b36da668a1241ce2f0724f9c
AnmolSR98 <anmolratolcoding@gmail.com>
2024-06-03 21:10:19 -0600

    Add files via upload

--- BinarySearchTree/LLRBT.java
+++ BinarySearchTree/LLRBT.java
@@ -2,12 +2,12 @@ package LLRBT;
 
 public class LLRBT<T extends Comparable>{
 	
-	class Node{
+	public class Node{
 		private Node right;
 		private Node left;
 		private T data;
 		private Node parent;
-		private boolean colour = false; // all nodes start off black
+		private String colour = "r"; // all nodes start off red
 		
 		
 		public Node(T dataToStore) {data = dataToStore;}
@@ -15,12 +15,20 @@ public class LLRBT<T extends Comparable>{
 		public Node getRight() { return right;}
 		public Node getLeft() { return left;}
 		public Node getParent() {return parent;}
-		public boolean getColour() {return colour;}
+		public String getColour() {return colour;}
 		
 		public void setRight(Node newRight) {right = newRight;}
 		public void setLeft(Node newLeft) {left = newLeft;}
 		public void setParent(Node newParent) {parent = newParent;}
-		public void setColour() {colour = !colour;}
+		public void setColour() {
+			if (colour.compareTo("r") == 0){
+				colour = "b";
+			}
+			
+			else {
+				colour = "r";
+			}
+		}
 		
 		public T getData(){return data;}
 		public void setData(T newData) {data = newData;}  
@@ -67,7 +75,76 @@ public class LLRBT<T extends Comparable>{
 	}
 	
 	public void add(T value) {
-		root = recAdd(value, root);
+
+		// case 1, empty tree
+		if (root == null) {
+			Node newNode = new Node(value);
+			newNode.setColour(); // changing colour to black
+			root = newNode;
+		}
+		
+		else {
+			Node newNode = new Node(value);
+			Node parent = getParent(value);
+			Node pibling = getSibling(parent);
+			
+			// case 2, parent and pibling are red
+			if ((parent.getColour().compareTo("r") == 0) && (pibling.getColour().compareTo("r") == 0)) {
+				parent.setColour();
+				pibling.setColour();
+				Node grandParent = parent.getParent();
+				grandParent.setColour();
+				
+			}
+			
+			else {
+				if (pibling == null) {
+					
+				}
+			}
+			
+			
+		}
+	}
+	
+	private void Case2(Node parent) {
+		
+	}
+	
+	private Node getParent(T value) {
+		Node tmp = root;
+		Node parent = tmp;
+		
+		while (tmp != null) {
+			if (tmp.getData().compareTo(value) < 0) {
+				parent = tmp;
+				tmp = tmp.getLeft();
+			}
+			
+			else {
+				parent = tmp;
+				tmp = tmp.getRight();
+			}
+		}
+		
+		return parent;
+	}
+	
+	private Node getSibling(Node node) {
+		if (node.getParent() == null)  {
+			return null;
+		}
+		
+		else {
+			// if the parent is the right child
+			if (node.getParent().getData().compareTo(node.getData()) < 0) {
+				return node.getParent().getLeft();
+			}
+			
+			else {
+				return node.getParent().getRight();
+			}
+		}
 	}
 	
 	private Node recAdd(T value, Node root) {
---
+++ BinarySearchTree/RBTTester.java
@@ -0,0 +1,11 @@
+package LLRBT;
+
+public class RBTTester {
+	
+	public static void main(String[] args) throws Exception {
+		LLRBT searchTree = new LLRBT();
+		
+		// bug with remove function [FIXED]
+	}
+	
+}
