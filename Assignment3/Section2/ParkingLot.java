beec13523ad4e1e5ab58bbcf768dade72f7e6476
AnmolSR98 <anmolratolcoding@gmail.com>
2024-06-09 15:04:44 -0600

    Add files via upload

--- Assignment3/Section2/ParkingLot.java
+++ Assignment3/Section2/ParkingLot.java
@@ -1,52 +1,68 @@
-package Section3;
+// Anmol Ratol 30231177
+// A java file for the parkingLot class, which organizes car parking spots using a hash table
+package Section2;
 
 import java.util.LinkedList;
 
 public class ParkingLot  {
 	
+	// the actual array of linked lists of cars
 	private LinkedList[] parkingLotArray;
-	private int size;
-	private int arrayLength = 20;
 	
+	// the size of the parking lot
+	private int listLength;
+	
+	// nested class for car
 	class Car{
+		// basic attributes of the car
 		String licencePlate;
 		String parkingSpot;
 		
+		// a string method for cleaner printing
 		@Override
 		public String toString() {
 			return licencePlate + ":" + parkingSpot;
 		}
 	}
 	
-	public ParkingLot() {
-		parkingLotArray = new LinkedList[arrayLength];
-		for (int i = 0; i < arrayLength; i++) {
-			parkingLotArray[i] = new LinkedList<Car>();
+	// constructor for the parking lot, requires lotSize as an argument, and then fills parkingLotArray with that many empty linkedLists
+	public ParkingLot(int lotSize) {
+		listLength = lotSize;
+		parkingLotArray = new LinkedList[listLength];
+		for (int i = 0; i < listLength; i++) {
+			parkingLotArray[i] = new LinkedList<>();
 		}
 	}
 	
+	// hash function, take the hashCode of the licence plate and mod it by listLength
 	public int Hash(String licencePlate) {
-		return licencePlate.hashCode() % arrayLength;
+		return licencePlate.hashCode() % listLength;
 	}
 	
-	
+	// search function
 	public boolean Search(String licencePlate) {
 		
+		// first get the location using hash function
 		LinkedList lotLocation = parkingLotArray[Hash(licencePlate)];
 		
+		// then cycle through the linked list and if the licencePlate is found, return true
 		for (int i = 0; i < lotLocation.size(); i++) {
-			Car immCar = (ParkingLot.Car)lotLocation.get(i);
-			if (immCar.licencePlate.compareTo(licencePlate) == 0) {
+
+			if (((Car)lotLocation.get(i)).licencePlate.compareTo(licencePlate) == 0) {
 				return true;
 			}
 		}
 		
+		// otherwise, return false
 		return false;
 	}
 	
+	// insert function
 	public void Insert(String licencePlate, String parkingSpot) {
 		
+		// provided that the car is not in the lot already
 		if (!Search(licencePlate)) {
+			// create a new car and it to ParkingLot.the lot in its calculated hash location
 			LinkedList lotLocation = parkingLotArray[Hash(licencePlate)];
 			Car carToAdd = new Car();
 			carToAdd.licencePlate = licencePlate;
@@ -55,10 +71,11 @@ public class ParkingLot  {
 		}
 		
 		else {
+			// otherwise find the car and update its parkingSpot data
 			LinkedList lotLocation = parkingLotArray[Hash(licencePlate)];
 			
 			for (int i = 0; i < lotLocation.size(); i++) {
-				Car immCar = (ParkingLot.Car)lotLocation.get(i);
+				Car immCar = (Car)lotLocation.get(i);
 				if (immCar.licencePlate.compareTo(licencePlate) == 0) {
 					immCar.parkingSpot = parkingSpot;
 				}
@@ -67,13 +84,16 @@ public class ParkingLot  {
 		
 	}
 	
-	public String Retrieve(String licencePlate) {
+	// retrieve function to get the parking spot of a car with this specific licence plate
+	public String Retrieve(String licencePlate) throws Exception {
 		
+		// if it is in the lot
 		if (Search(licencePlate)) {
+			// find it and return its parking spot
 			LinkedList lotLocation = parkingLotArray[Hash(licencePlate)];
 			
 			for (int i = 0; i < lotLocation.size(); i++) {
-				Car immCar = (ParkingLot.Car)lotLocation.get(i);
+				Car immCar = (Car)lotLocation.get(i);
 				if  (immCar.licencePlate.compareTo(licencePlate) == 0)  {
 					return immCar.parkingSpot;
 				}
@@ -81,14 +101,16 @@ public class ParkingLot  {
 			
 		}
 		
-		System.out.println("Car is not in the table!");
-	
-		return null;
+		// otherwise, raise an exception
+		throw new Exception("Car is not in the table!");
 	}
 	
-	public void Delete(String licencePlate) {
+	public void Delete(String licencePlate) throws Exception {
 		
+		// if it is in the lot
 		if (Search(licencePlate)) {
+			
+			// find it, and remove the element from the list
 			LinkedList lotLocation = parkingLotArray[Hash(licencePlate)];
 			
 			for (int i = 0; i < lotLocation.size(); i++) {
@@ -99,15 +121,18 @@ public class ParkingLot  {
 			}
 		}
 		
+		// otherwise, raise an exception
 		else {
-			System.out.println("Car is not in the table!");
+			throw new Exception("Car is not in the table!");
 		}
 	}
 	
+	// a function to get the parking lot as a string 
 	@Override
 	public String toString() {
 		String thisLevel = "";
-		for (int i = 0; i < arrayLength; i++) {
+		// incrementally building up the string with each linked list being put on its own line
+		for (int i = 0; i < listLength; i++) {
 			thisLevel += String.format("%d: ", i);
 			LinkedList tmp = parkingLotArray[i];
 			thisLevel += tmp + "\n";
--- Assignment3/Section2/ParkingLotManager.java
+++ Assignment3/Section2/ParkingLotManager.java
@@ -1,44 +1,71 @@
-package Section3;
+// Anmol Ratol 30231177
+// A java file for the parkingLotManager class, essentially a driver for the parkingLot class
+package Section2;
+
 
 public class ParkingLotManager {
 	
+	// main method
 	public static void main(String[] args) {
 		
-		ParkingLot testLot = new ParkingLot();
+		// instantiating the testLot, being size 20 
+		ParkingLot testLot = new ParkingLot(20);
 		
-		// 1.
+		// 1. Adding the various cars using the park function to quickly split the strings  
 		park(testLot, "AB1234:1-23");
 		park(testLot, "CD5678:2-45");
 		park(testLot, "EF9012:3-12");
 		park(testLot, "GH3456:1-34");
 		
-		// 2.
+		// 2. Printing the initial state of the lot
 		System.out.println(testLot);
 		
-		// 3.
+		// 3. Inserting a car that was already there, which should do nothing more than update its lot number
 		testLot.Insert("AB1234", "2-50");
 		
-		// 4.
+		// 4. Search for the value of AB1234
 		testLot.Search("AB1234");
 		
-		// 5.
-		System.out.println(testLot.Retrieve("CD5678"));
+		// 5. Trying out the retrieve function for CD5678, expected output should be 2-45 with no exceptions raised
+		try {
+			System.out.println(testLot.Retrieve("CD5678"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 		
-		// 6.
-		testLot.Delete("CD5678");
+		// 6. Deleting CD5678, no exceptions should be raised
+		try {
+			testLot.Delete("CD5678");
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 		
-		// 7.
-		testLot.Delete("CD5678");
+		// 7. Attempting to delete it a second time, an exception should be raised and then printed
+		try {
+			testLot.Delete("CD5678");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 		
-		// 8.
-		testLot.Retrieve("CD5678");
+		// 8. Attempting to retrieve CD5678, an exception should be raised and then printed
+		try {
+			testLot.Retrieve("CD5678");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 		
-		// 9.
+		// 9. Print the final status of the lot
 		System.out.println(testLot);
 		
 		
 	}
 	
+	// quick function to insert the cars that splits the strings for me
 	public static void park(ParkingLot lot, String licenceAndLot) {
 		lot.Insert(licenceAndLot.split(":")[0], licenceAndLot.split(":")[1]);
 	}
