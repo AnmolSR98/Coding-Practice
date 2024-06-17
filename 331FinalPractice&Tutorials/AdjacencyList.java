72dc4e9e9b1339bcfd57c1def486be8e2054e1d8
AnmolSR98 <anmolratolcoding@gmail.com>
2024-06-17 14:16:39 -0600

    Add files via upload

--- 331FinalPractice&Tutorials/AdjacencyList.java
+++ 331FinalPractice&Tutorials/AdjacencyList.java
@@ -1,6 +1,9 @@
 package mygraph;
 
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class AdjacencyList {
 	
@@ -33,16 +36,79 @@ public class AdjacencyList {
 		return false;
 	}
 	
-	public void getSpanningTree() {
+	public List<Integer> DFS(int startNode) {
+		
+		List<Integer> vertices = new ArrayList<Integer>();
+		Stack<Integer> verticesStack = new Stack<Integer>();
+	
+		
+		verticesStack.push(startNode);
+		
+		while (!verticesStack.isEmpty()) {
+			
+			int currentVertex = verticesStack.pop();
+			if (!vertices.contains(currentVertex)) {
+				vertices.add(currentVertex);
+			}
+			LinkedList<Integer[]> currentList = list[currentVertex]; 
+			
+			for (int i = 0; i < currentList.size(); i++) {
+				int node = currentList.get(i)[0];
+				verticesStack.push(node);
+			}
+		
+		}
+
+		return vertices;
+	}
+	
+	public List<Integer> BFS(int startNode) {
+		
+		List<Integer> vertices = new ArrayList<Integer>();
+		LinkedQueue verticesQueue = new LinkedQueue();
+	
+		
+		verticesQueue.enqueue(startNode);
+		
+		while (!verticesQueue.isEmpty()) {
+			
+			int currentVertex = (int) verticesQueue.dequeue();
+			if (!vertices.contains(currentVertex)) {
+				vertices.add(currentVertex);
+			}
+			LinkedList<Integer[]> currentList = list[currentVertex]; 
+			
+			for (int i = 0; i < currentList.size(); i++) {
+				int node = currentList.get(i)[0];
+				verticesQueue.enqueue(node);
+			}
+		
+		}
+
+		return vertices;
+	}
+	
+	// exercises to do for these
+	public boolean hasCycle() {
+		return false;
+	}
+	
+	public void TopologicalSort() {
 		
 	}
+	
+	public int[] shortestPath() {
+		return new int[5];
+	}
+	
+	// Exercise 4 is just BFS again
 	
 	public String toString() {
 		String returnString = "";
 		for (int i = 0; i < size; i++) {
 			returnString += (i + ": ") ;
 			for (int j = 0; j < list[i].size(); j++) {
-				returnString += ("["+list[i].get(i)[0] + ", "+list[i].get(i)[1]+"]"+", ") ;
+				returnString += ("["+list[i].get(j)[0] + ", "+list[i].get(j)[1]+"]"+", ") ;
 			}
 			returnString += "\n";
 		}
--- 331FinalPractice&Tutorials/GraphTester.java
+++ 331FinalPractice&Tutorials/GraphTester.java
@@ -3,10 +3,21 @@ package mygraph;
 public class GraphTester {
 	
 	public static void main(String[] args) {
-		AdjacencyList testGraph = new AdjacencyList(5);
+		
+		AdjacencyList testGraph = new AdjacencyList(6);
+		testGraph.addConnection(0, 1, 1);
+		testGraph.addConnection(0, 2, 1);
+		testGraph.addConnection(0, 3, 1);
 		testGraph.addConnection(0, 4, 1);
+		testGraph.addConnection(1, 3, 1);
+		testGraph.addConnection(3, 4, 1);
+		testGraph.addConnection(4, 2, 1);
+		testGraph.addConnection(4, 5, 1);
+		
 		System.out.println(testGraph);
-		System.out.println(testGraph.removeConnection(0, 4));
+		System.out.println(testGraph.DFS(0));
+		System.out.println(testGraph.BFS(0));
+		
 	}
 
 }
