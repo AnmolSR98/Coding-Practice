3440e38abe147e65697eae95daddec57c7afca74
AnmolSR98 <anmolratolcoding@gmail.com>
2024-09-02 17:20:08 -0600

    Add files via upload

--- C Exercises/Exercise18.c
+++ C Exercises/Exercise18.c
@@ -12,33 +12,97 @@ int getRightChild(int index) {
     return 2*index + 2;
 }
 
-bool recCheckTree(int* array, int root) {
+// check that the root is greater than its predecessor
+bool checkPredecessor(int* array, int root, int length) {
 
-    if (array[root] == NULL) {
+    int successor = getLeftChild(root);
+    bool loop = true;
+
+    while (loop) {
+        if (getRightChild(successor) < length) {
+            successor = getRightChild(successor);
+        }
+
+        else {
+            loop = false;
+        }
+    }
+
+    return (array[root] > array[successor]);
+}
+
+// check that the root is less than its successor
+bool checkSuccessor(int* array, int root, int length) {
+
+    int predecessor = getRightChild(root);
+    bool loop = true;
+
+    while (loop) {
+
+        if (getLeftChild(predecessor) < length) {
+            predecessor = getLeftChild(predecessor);
+        }
+
+        else {
+            loop = false;
+        }
+    }
+
+    return (array[root] < array[predecessor]);
+}
+
+bool recCheckTree(int* array, int root, int length) {
+
+    // if the array is null, return true
+    if (array[root] == 0) {
         return true;
     }
 
-    else if ((array[root] < array[getRightChild(root)]) && (array[root] > array[getLeftChild(root)])) {
-        recCheckTree(array, getLeftChild(root)); 
-        recCheckTree(array, getRightChild(root));   
-    }
-
+    // otherwise
     else {
-        return false;
+
+        // if both the children are null, return true
+        if ((getLeftChild(root) >= length) && (getRightChild(root) >= length)){
+            return true;
+        }
+        
+        // if the left child is null
+        else if ((getLeftChild(root) >= length)) {
+            
+            // check the right child
+            if (array[root] < array[getRightChild(root)]) {
+                return recCheckTree(array, getRightChild(root), length);
+            }
+        }
+
+        // if the right child is null
+        else if ((getRightChild(root) >= length)) {
+            
+            // check the left child
+            if (array[root] > array[getLeftChild(root)]) {
+                return recCheckTree(array, getLeftChild(root), length);
+            }
+        }
+
+        // otherwise if it has both children, check that the condition is upheld
+        else if ((array[root] > array[getLeftChild(root)]) && (array[root] < array[getRightChild(root)])) {
+            return recCheckTree(array, getLeftChild(root), length) && recCheckTree(array, getRightChild(root), length);
+        }
     }
 
-    return true;
+    return false;
 }
 
 // function for checking if the tree is a valid binary search tree (for preorder traversal)
-bool isValidBST(int* tree) {
-    return recCheckTree(tree, 0);
+bool isValidBST(int* tree, int length) {
+    return recCheckTree(tree, 0, length) && checkPredecessor(tree, 0, length) && checkSuccessor(tree, 0, length);
 }
 
 
 int main() {
     
-    int testTree[7] = {5, 2, 7, 1, 4, 6, 10};
-    bool test = isValidBST(testTree);
+    int length = 7;
+    int testTree[7] = {7, 3, 9, 2, 4, 8, 10};
+    bool test = isValidBST(testTree, length);
     return 0;
 }
