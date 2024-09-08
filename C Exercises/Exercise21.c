9c6e3036e17f5c25d6404e0efb1f2e7e73662827
AnmolSR98 <anmolratolcoding@gmail.com>
2024-09-07 18:17:33 -0600

    Add files via upload

--- C Exercises/Exercise21.c
+++ C Exercises/Exercise21.c
@@ -1,33 +1,63 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-// quickly writing an absolute value function
-int getAbs(int a, int b) {
+// TRY TO FIX THIS LATER, TRY RADIX SORT
+// simple swap function
+void swap(int a, int b, int* nums) {
 
-    if (a <= b) {
-        return (b - a);
-    }
-
-    return (a - b);
 }
 
-// code to get the maximum gap between the two elements in an array, after it has been sorted
-int maximumGap(int* nums, int numsSize) {
+int getDigitAtPosition(int position, int number) {
 
-    if ((numsSize == 0) || (numsSize == 1)) {
-        return 0;
-    }
+    int valuesHigher = (number / (int) pow(10, position)) * (int) pow(10, position);
 
-    // might be faster to use binary search tree, with modded method
-    // or start from either end
+    number = number - valuesHigher;
+    
+    number = number / ((int) pow(10, (int) log10(number))); 
 
-
-    return 0;
+    return number;
 }
 
+int getMax(int* nums, int numsSize) {
+    int i, max = -2000000000;
+    for (i = 0; i < numsSize; i++) {
+        if (max < nums[i]) {
+            max = nums[i];
+        }
+    }
+
+    return max;
+}
+
+/**
+// implementing radix sort
+void radixSort(int* nums, int numsSize) {
+
+    int max = getMax(nums, numsSize);
+    int intLength = (int) log10(max);
+    int i, j;
+
+    for (i = 1; i < intLength; i++) {
+        for (j = 0; j < numsSize; j++) {
+
+        }
+    }
+
+
+}
+**/
 
 int main() {
     
+    int test1[10] = {12, 18, 3, 10, 5, 7, 11, 2, 6, 20}; // 6
+    int test2[10] = {11, 16, 19, 1, 15, 9, 4, 5, 14, 20}; // 4
+    int test3[10] = {10, 2, 6, 3, 14, 5, 12, 16, 15, 11}; // 4
+
+    int gap1 = getDigitAtPosition(4, 1723);
+
+    printf("Digit is: %d\n", gap1);
+
     return 0;
 }
