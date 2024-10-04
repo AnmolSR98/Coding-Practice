54e8fb6b6d2d65059433cffd909e21de8da5e7c5
AnmolSR98 <anmolratolcoding@gmail.com>
2024-10-04 11:48:43 -0600

    Update optimal.c
    Made a version that looks to see which pageNumber either appears last or not at all, with a bias towards the later frames.

---
+++ CPSC457/A2/optimal.c
@@ -0,0 +1,105 @@
+#include "main.h"
+
+// look at write backs again for both FIFO and LRU
+// definitely change, should only writeback for dirty bits
+
+// function to the frame to update
+// need to account for the page that will be doing the replacing
+int indexOfNextInstance(page** pageArray, int lowerBound, int length, int pageNumber) {
+
+    int i;
+    // cycle through and return the very first instance of the page number
+    for (i = lowerBound + 1; i < length; i++) {
+
+        if (pageArray[i]->pageNumber == pageNumber) {
+            return i;
+        }
+    }
+    
+    // return -1 if the page doesn't show up again
+    return (-1);
+}
+
+int getFrameToUpdateOptimal(frame** frameArray, page** pageArray, int numFrames, int numPages, int pageNumber, int currentIndex) {
+
+    int i;
+    int frameIndex;
+    int lastestAppearance = 0;
+    int potential;
+    
+    // loop to cycle through the various frames to see if any of them are free
+    for (i = 0; i < numFrames; i++) {
+        // return if one is found
+        if (frameArray[i]->currentPage == NULL) {
+            frameArray[i]->pageFaults += 1;
+            return i;
+        }
+
+        else if (frameArray[i]->currentPage->pageNumber == pageNumber) {
+            return i;
+        }
+
+        // check if that frame is the oldest
+        potential = indexOfNextInstance(pageArray, numPages, frameArray[i]->currentPage->pageNumber, currentIndex);
+
+        if ((potential > lastestAppearance) || (potential == -1)) {
+            lastestAppearance = potential;
+            frameIndex = i;
+        }
+    }
+
+    // update the page fault if that specific page number was not found in memory
+    frameArray[frameIndex]->pageFaults += 1;
+    
+    // if no free page was found then return the oldest frame
+    return frameIndex;
+}
+
+void fifo(page** pageArray, int numFrames, int numPages) {
+
+    // creating a new array of frames and filling it
+    frame** frameArray = malloc(sizeof(frame) * numFrames);
+    int i;
+    for (i = 0; i < numFrames; i++) {
+        frameArray[i] = createFrame(i+1);
+    }
+
+    int frameToUpdate;
+    page* newPage = (page*) malloc(sizeof(page));
+    frame* currentFrame = (frame*) malloc(sizeof(frame));
+    int totalTimesWasInMemory = 0;
+
+    // now moving onto actually simulating checking the frames
+    for (i = 0; i < numPages; i++) {
+
+        // get the current page from the queue
+        newPage = pageArray[i];
+
+        // gonna create a function to check for the index of the first free frame
+        currentFrame = frameArray[getFrameToUpdateOptimal(frameArray, pageArray, numFrames, numPages, newPage->pageNumber, i)];
+
+        // adding on a update if the frame was null before or if the current page has a dirty bit
+        // in the case of a null frame or change in page number, add on a write back
+        if ((currentFrame->currentPage == NULL) || (currentFrame->currentPage->pageNumber != newPage->pageNumber)) {
+            currentFrame->totalWriteBacks += 1;
+            currentFrame->timeArrived = i;
+        }
+
+        // also do this in the case for identical page numbers but with a dirty bit
+        else if ((currentFrame->currentPage->pageNumber == newPage->pageNumber) && (newPage->dirty == 1)) {
+            currentFrame->totalWriteBacks += 1;
+        }
+
+        if(currentFrame->currentPage!= NULL) {
+            if (currentFrame->currentPage->pageNumber == newPage->pageNumber) {
+                totalTimesWasInMemory++;
+            }
+        }
+
+        // updating the frame
+        currentFrame->currentPage = newPage;
+    }
+
+    printTable(frameArray, numFrames);
+    printf("%d\n", totalTimesWasInMemory);
+}
