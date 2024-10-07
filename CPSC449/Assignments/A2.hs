697077b8123e8f8873f9dc81007b8441588d1c55
AnmolSR98 <anmolratolcoding@gmail.com>
2024-10-07 15:05:40 -0600

    some comments made

--- CPSC449/Assignments/A2.hs
+++ CPSC449/Assignments/A2.hs
@@ -1,11 +1,14 @@
+-- Anmol Ratol, 30231177
 -- Question 1: Transpose a Matrix
 transposeMatrix :: [[a]] -> [[a]]
 transposeMatrix x = transposer (x) (0) 
 
+-- helper function that recursively uses the number of columns to determine when the process is done
 transposer :: [[a]] -> Int -> [[a]]
 transposer x y | (y == length(x !! 0)) = []
                | otherwise = [getArrayOfColumnN (x) (y)] ++ transposer (x) (y+1)
 
+-- function that returns an array of the nth element of each column
 getArrayOfColumnN :: [[a]] -> Int -> [a]
 getArrayOfColumnN [] y = []
 getArrayOfColumnN (x:xs) y = [(x !! y)] ++ getArrayOfColumnN (xs) (y)
