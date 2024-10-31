4c6d6609ab5fca255d8318fb6156e1dda0a6cc5f
AnmolSR98 <anmolratolcoding@gmail.com>
2024-10-31 09:58:05 -0600

    pretty much done A3.hs

--- CPSC449/Assignments/A3.hs
+++ CPSC449/Assignments/A3.hs
@@ -1,3 +1,5 @@
+-- Question 1
+
 processNestedList:: [[Int]] -> [Int]
 processNestedList = map (\x -> x*x) . filter (even) . nestedHelper
 
@@ -5,6 +7,13 @@ nestedHelper :: [[Int]] -> [Int]
 nestedHelper [] = []
 nestedHelper (x:xs) = x ++ nestedHelper xs
 
+fst' :: (a, b) -> a
+fst' = (\(x, y) -> x)
+
+snd' :: (a, b) -> b
+snd' = (\(x, y) -> y)
+
+-- Question 2
 
 matrixAddition:: [[Int]] -> [[Int]] -> [[Int]]
 matrixAddition = zipWith (\x y -> addHelper x y)
@@ -26,5 +35,42 @@ matrixTrace = foldl (+) (0) . traceHelper 0
 traceHelper :: Int -> [[Int]] -> [Int]
 traceHelper index [] = []
 traceHelper index (x:xs) = [x !! index] ++ traceHelper (index+1) (xs)
+
+-- Question 3
+
+class Shape a where
+    area :: a -> Double
+
+data Circle = Circle Double
+data Rectangle = Rectangle Double Double
+
+instance Shape Circle where
+    area (Circle r) = pi * r * r
+
+instance Shape Rectangle where
+    area (Rectangle x y) = x * y
+
+-- Question 4
+
+data AVLTree a = Empty | Node a (AVLTree a) (AVLTree a)
+
+-- getting the height of the tree
+heightAVL :: AVLTree a -> Int
+heightAVL Empty = -1
+heightAVL (Node a Empty Empty) = 0
+heightAVL (Node a l r) = 1 + max (heightAVL l) (heightAVL r) 
+
+-- getting some sort of error with regards to comparison on this one [FIXED]
+isOrdered :: (Ord a) => AVLTree a -> Bool
+isOrdered Empty = True
+isOrdered (Node a Empty Empty) = True
+isOrdered (Node a Empty (Node r sl sr)) = (a < r) && isOrdered (Node r sl sr)
+isOrdered (Node a (Node l sl sr) Empty) = (a > l) && isOrdered (Node l sl sr)
+isOrdered (Node a (Node l ll lr) (Node r rl rr)) = (a < r) && (a > l) && isOrdered (Node l ll lr) && isOrdered (Node r rl rr)
+
+-- need to check that it is balanced
+isBalanced:: AVLTree a -> Bool
+isBalanced Empty = True
+isBalanced (Node a l r) = (abs(heightAVL l - heightAVL r) <= 1)  
 
 
