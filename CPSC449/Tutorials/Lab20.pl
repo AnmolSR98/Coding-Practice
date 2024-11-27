a3e870e7f924bd8e78cca03148d8ac1f239de01d
AnmolSR98 <anmolratolcoding@gmail.com>
2024-11-26 18:10:28 -0700

    Create Lab20.pl

---
+++ CPSC449/Tutorials/Lab20.pl
@@ -0,0 +1,57 @@
+% Exercises:
+% Facts:
+
+likes(susan, pasta).
+likes(susan, ice_cream).
+likes(susan, sushi).
+likes(jane, chocolate).
+likes(jane, cookie).
+likes(john, pizza).
+likes(john, pasta).
+likes(mary, salad).
+likes(mary, pasta).
+likes(tom, pizza).
+likes(tom, sushi).
+likes(tom, cookie).
+
+% Using the facts above, write queries for questoins below:
+
+% Who likes pasta?
+
+% What does Susan like?
+
+
+% Attributes of foods
+healthy(salad).
+healthy(sushi).
+healthy(pasta).
+unhealthy(pizza).
+unhealthy(cookie).
+
+sweet(ice_cream).
+sweet(cookie).
+
+% Write the following queries:
+% List all people who like unhealthy food:
+
+% List all people who like unhealthy food:
+
+% Who likes at least one healthy and one unhealthy food?:
+
+
+% Conjuction (AND)
+% Write a rule that determines if someone likes healthy food or not.
+
+
+% Disjunction (OR)
+% Write a rule that determines if someone likes unhealthy or swee food.
+
+
+% Define a rule to find people who like varied foods, meaning that they like more than one type (healthy or unhealthy) of food
+
+
+% Define a rule to find people who like only healthy foods
+%  The \+ operator is used for negation, meaning "not". 
+
+
+% Define a rule that finds people who like more than one type of food but don’t like both healthy and unhealthy.
