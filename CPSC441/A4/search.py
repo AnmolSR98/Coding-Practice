75c76386115e98d66b2e8ac40cc8926c89e339b6
AnmolSR98 <anmolratolcoding@gmail.com>
2025-04-03 21:57:48 -0600

    commented up search,py
    asjdksd

--- CPSC441/A4/search.py
+++ CPSC441/A4/search.py
@@ -5,14 +5,17 @@ import math
 def dijkstra(adjMatrix, source):
     vertices, dist, prev = [] , [] , []
 
+    # set up the vertices, dist, and prev arrays
     for i in range(len(adjMatrix[0])):
         prev.append(-1)
         dist.append(math.inf)
         vertices.append(i)
     dist[source] = 0
 
+    # while all vertices have yet to be checked
     while len(vertices) != 0:
 
+        # get the vertex with the min distance from the source
         minDistVertex = -1
         minDist = math.inf
 
@@ -23,19 +26,21 @@ def dijkstra(adjMatrix, source):
 
         vertices.remove(minDistVertex)
 
-        # now getting the values for all the neighbours
+        # get all of its neighbours
         neighbours = []
         adjMatrix[minDistVertex]
         for i in range(len(adjMatrix[minDistVertex])):
             if adjMatrix[minDistVertex][i] != 0:
                 neighbours.append(i)
 
+        # check if the path from this node to the neighbour is the best so far, and update it if it is
         for neighbour in neighbours:
             alt = dist[minDistVertex] + adjMatrix[minDistVertex][neighbour]
             if alt < dist[neighbour]:
                 dist[neighbour] = alt
                 prev[neighbour] = minDistVertex
 
+    # return the distances and previous arrays
     return dist, prev
 
 def bellmanFord(adjMatrix, source):
@@ -44,8 +49,9 @@ def bellmanFord(adjMatrix, source):
     prev = [-1] * len(adjMatrix)
     dist[source] = 0
 
-    # check through all edges FIX
+    # check through n - 1 times (n = number of nodes)
     for i in range(len(adjMatrix) - 1):
+        # for each node, check its neighbours and update if it is the best path found thus far
         for j in range(len(adjMatrix)):
             for k in range(len(adjMatrix[source])):
                 if adjMatrix[j][k] != 0:
@@ -56,33 +62,43 @@ def bellmanFord(adjMatrix, source):
     # check for negative weight cycles
     for j in range(len(adjMatrix)):
         for k in range(len(adjMatrix[source])):
+            # for each node, check through its edgees
             if adjMatrix[j][k] != 0:
+                # if a negative cycle exists
                 if dist[j] + adjMatrix[j][k] < dist[k]:
+                        # mark it as the previous node 
                         prev[k] = j
                         # keeping track of visited nodes
                         visited = [False] * len(adjMatrix)
+                        # mark the node itself as visited
                         visited[k] = True
+                        # go through all the previous nodes
                         while not visited[j]:
                             visited[j] = True
                             j = prev[j]
+                        # now add these to the negative cycle and return it
                         negativeCycle = [j]
                         k = prev[j]
                         while k != j:
                             negativeCycle = [k] + negativeCycle
-                            k = prev[k]        
+                            k = prev[k]     
+                        return negativeCycle
 
     return dist, prev
 
 def getPath(prev, source, dest):
+    # base case
     if source == dest:
         return [source]
     
+    # otherwise, start at the destination
     path = []
     currentNode = dest
+    # move continuously backwards using the prev array to update
     while currentNode != source:
         path = [currentNode] + path
         currentNode = prev[currentNode]
 
+    # now add the source as the final addition and return
     path = [source] + path
-
     return path
